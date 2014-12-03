c2618c2d88ad4f69e303e93416b9db89a323608b
Josephine <jossan_cuellar@hotmail.com>
2014-12-03 20:15:33 +0100

    New variables

--- H2_main.c
+++ H2_main.c
@@ -19,14 +19,13 @@ int main(){
 	double sum, sum2;
 	int N; // Number of interations
 	double mean, mean2, var; // <f>, <f^2> and var[f]
-	double x[N];
-	double p[N];
 	double delta;
 	double q;
 	int throw_away, rejections; // Number of iterations to throw away in the begining, number of rejections
-	double r; // Random number [0,1]
+	double random[2][3]; // Random number [0,1]
 	double alpha;
 	double positions[2][3]; // Positions in 3D for 2 particles
+	double p[2][3]; // Probabilities
 
 	// Open a file to print the variable x in
 	FILE *m_file;
@@ -36,7 +35,6 @@ int main(){
 	sum = 0;
 	sum2 = 0;
 	var = 0;
-	x[0] = 0.5;
 	p[0] = sin(PI * x[0]);
 	delta = 0.45;
 	throw_away = 2000;
@@ -44,6 +42,13 @@ int main(){
 	alpha = 0.1;
 	N = 10000;
 
+	// Initialize positions
+	for(i = 0; i < 3; i++){
+		positions[0][i] = 1.0;
+		positions[1][i] = -1.0;
+	}
+
+	// Save initial positions
 	fprintf(m_file,"%F \n", x[0]);
 		
 	int n = 0;
@@ -52,8 +57,8 @@ int main(){
 	for(j = 1; j < N; j++){
 
 		// Generate random number and get next state x
-		r = (double) rand() / (double) RAND_MAX;	
-		x[j] = x[j-1] + delta*(r - 0.5);
+		random = (double) rand() / (double) RAND_MAX;	
+		x[j] = x[j-1] + delta*(random - 0.5);
 
 		// Calculate the probability
 		p[j] = 0.0;
@@ -61,7 +66,7 @@ int main(){
 		q = p[j]/p[j-1];
 
 		// New random number
-		r = (double) rand() / (double) RAND_MAX;
+		random = (double) rand() / (double) RAND_MAX;
 
 		// Trial
 		if(q < r){
