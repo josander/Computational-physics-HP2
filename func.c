6192afe46731d957a9ed16576106f5bf36d87429
David <david.emil.svensson@gmail.com>
2014-12-03 20:27:31 +0100

    added getDist

--- func.c
+++ func.c
@@ -8,3 +8,18 @@ Contains functions for homeproblem 2/b
 #include <stdio.h>
 #include <math.h>
 #define PI 3.141592653589
+
+double getDistance(double positions[][3]){
+	double distance = 0;
+	int i;
+
+
+	for(i = 0; i < 3; i++){
+		distance += pow(positions[0][i] - positions[1][i], 2);
+	}
+		
+	return (sqrt(distance));	
+
+
+}
+
