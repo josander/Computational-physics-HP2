2919c3ade9489fd663308ace6970734af8e9255d
Josephine <jossan_cuellar@hotmail.com>
2014-12-03 20:54:19 +0100

    Now it works

--- H2_main.c
+++ H2_main.c
@@ -21,11 +21,12 @@ int main(){
 	double delta;
 	double q;
 	int throw_away, rejections; // Number of iterations to throw away in the begining, number of rejections
-	double random[2][3]; // Random number [0,1]
+	double random; // Random number [0,1]
 	double alpha;
 	double positions[2][3]; // Positions in 3D for 2 particles
 	double temp[2][3]; // Temporary array for new positions
 	double p[2][3]; // Probabilities
+	double distance; 
 
 	// Initialize variables
 	sum = 0;
@@ -46,42 +47,53 @@ int main(){
 		p[1][i] = 0;
 	}
 
+	// Get initial distances
+	distance = 1;//getDistance(positions);
+
 	// Open a file to print the variable x in
 	FILE *m_file;
-	m_file = fopen("positions.data","w");
+	m_file = fopen("distance.data","w");
 
 	// Save initial positions
-	fprintf(m_file,"%F \t %F \t %F  \t %F \t %F \t %F \n", positions[0][0], positions[0][1], positions[0][2], positions[1][0], positions[1][1], positions[1][2]);
+	fprintf(m_file,"%F \n", distance);
 
 	// Calculate the integral
 	for(j = 1; j < N; j++){
 
-		// Generate random number and get next configuration
+		// Generate random numbers and get next configuration
 		for(i = 0; i < 3; i++){
-			random[0][i] = (double) rand() / (double) RAND_MAX;	
-			temp[0][i] = positions[][i] + delta*(random[][] - 0.5);
+			random = (double) rand() / (double) RAND_MAX;	
+			temp[0][i] = positions[0][i] + delta*(random - 0.5);
+
+			random = (double) rand() / (double) RAND_MAX;	
+			temp[1][i] = positions[1][i] + delta*(random - 0.5);
 		}
 
+		// Calculate distance between the particles
+		distance = 1;//getDistance(positions);
+
 		// Calculate the probability
-		p[j] = 0.0;
-		p[j] = sin(PI * x[j]);
-		q = p[j]/p[j-1];
+		p[0][j] = sin(PI * distance);
+		q = p[0][j]/p[0][j-1];
 
 		// New random number
 		random = (double) rand() / (double) RAND_MAX;
 
-		// Trial
-		if(q < r){
-			x[j] = x[j-1];
-			p[j] = p[j-1];
+		// Trial, if q >= random, save the temporary positions
+		if(q >= random){
+			for(i = 0; i < 3; i++){
+				positions[0][i] = temp[0][i];
+				positions[1][i] = temp[1][i];
+			}
+
+			p[0][j] = p[0][j-1];
 			rejections++;
 		}
 		
 		// Skip the 'throw_away' first datapoints
 		if(j > throw_away){
-			n++;
-			sum += x[j] * (1-x[j]) * 2.0 / sin(PI * x[j]) / PI;
-			sum2 += x[j] * (1-x[j]) * 2.0 / sin(PI * x[j]) / PI * x[j] * (1-x[j]) * 2.0 / sin(PI * x[j]) / PI;
+			sum += distance * (1-distance) * 2.0 / sin(PI * distance) / PI;
+			sum2 += distance * (1-distance) * 2.0 / sin(PI * distance) / PI * distance * (1-distance) * 2.0 / sin(PI * distance) / PI;
 		}
 
 	}
@@ -99,7 +111,7 @@ int main(){
 
 	// Print x to distribution.data
 	for(j = 0; j < N; j++){
-		fprintf(m_file,"%F \n", x[j]);
+		fprintf(m_file,"%F \n", distance);
 	}
 
 	// Close the data-file
--- func.c
+++ func.c
@@ -9,15 +9,17 @@ Contains functions for homeproblem 2/b
 #include <math.h>
 #define PI 3.141592653589
 
+// Function that returns a distance between two particles
 double getDistance(double positions[][3]){
+	
 	double distance = 0;
 	int i;
 
-
+	// Get the differences squared in each dimension
 	for(i = 0; i < 3; i++){
 		distance += pow(positions[0][i] - positions[1][i], 2);
 	}
-		
+	
 	return (sqrt(distance));	
 
 
