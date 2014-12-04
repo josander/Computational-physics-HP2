2d467dbdfa6e4331243f309530b807d7cdcd3974
Josephine <jossan_cuellar@hotmail.com>
2014-12-04 16:34:49 +0100

    Plot energies in matlab

--- plot_H2.m
+++ plot_H2.m
@@ -9,6 +9,7 @@ distNuc = importdata('distances.data');
 % Plot a histogram of the distances to the nucleus
 nbins = 40;
 
+figure(1);
 subplot(2,1,1);
 hist(distNuc, nbins);
 xlabel('Distance to the nucleus', 'fontsize', 12);
@@ -23,3 +24,15 @@ subplot(2,1,2);
 plot(r,f(r));
 xlabel('Distance to the nucleus', 'fontsize', 12);
 title('Approximated function', 'fontsize', 12);
+
+%% Plot energies
+
+energy = importdata('energy.data');
+
+figure(2);
+clf
+plot(energy(:,1), 'b');
+hold on
+plot(energy(:,2), 'r');
+xlabel('Datapoints', 'fontsize', 12);
+ylabel('Energy', 'fontsize', 12);
